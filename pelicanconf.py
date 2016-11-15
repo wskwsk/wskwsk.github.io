e6b66e5df7d81c52ebb0d4b878aac34539325506
wskwsk <wsk@wsk.com>
2016-11-15 14:30:40 +0100

    some improvements

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,9 @@ PATH = 'content'
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
+THEME = 'themes\pelican-bootstrap3'
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['tag_cloud']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -23,13 +26,26 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'http://github.com/wskwsk')
+          ,)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+STATIC_PATHS = ['images', 'pdfs']
+PYGMENTS_STYLE='solarizeddark'
+CC_LICENSE='CC-BY-SA'
+DISPLAY_TAGS_ON_SIDEBAR = True
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+RECENT_POST_COUNT = 5
+
+#important: THEME,PLUGIN,ARTICLE_SAVE_AS,ARTICLE_URL
+# in /content/pages i can keep static pages
+# display_tags requires tag_cloud plugin
